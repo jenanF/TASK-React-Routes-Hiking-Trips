643d52e712f837d4ec8e3fa34134bcfe8e7c676d
jenanF <108689302+jenanF@users.noreply.github.com>
2024-11-24 15:42:15 +0300

    done

--- src/components/TripDetail.js
+++ src/components/TripDetail.js
@@ -1,50 +1,58 @@
 import React from 'react';
 import tripsData from '../tripsData';
 import { useParams } from 'react-router';
+import { Navigate } from 'react-router';
 
 function TripDetail() {
   const { tripId } = useParams();
-  const trip = tripsData[tripId - 1];
-  return (
-    <div className="modal-dialog modal-xl">
-      <div className="modal-content">
-        <div className="modal-body text-center pb-5">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-8">
-                <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">
-                  {trip.name}
-                </h2>
-                <div className="divider-custom">
-                  <div className="divider-custom-line"></div>
-                  <div className="divider-custom-icon">
-                    <i className="fas fa-star"></i>
+  const trip = tripsData[tripId ? tripId - 1 : -1];
+
+  if (!trip) {
+    return <Navigate to="/" />
+  } else {
+    return (
+      <div className="modal-dialog modal-xl">
+
+        <div className="modal-content">
+          <div className="modal-body text-center pb-5">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-lg-8">
+                  <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">
+                    {trip.name}
+                  </h2>
+                  <div className="divider-custom">
+                    <div className="divider-custom-line"></div>
+                    <div className="divider-custom-icon">
+                      <i className="fas fa-star"></i>
+                    </div>
+                    <div className="divider-custom-line"></div>
                   </div>
-                  <div className="divider-custom-line"></div>
+                  <img
+                    className="img-fluid rounded mb-5"
+                    src={trip.img}
+                    alt="..."
+                  />
+                  <p className="mb-4">
+                    City : {trip.city}
+                    <br />
+                    Length : {trip.length1}Km
+                    <br />
+                    Rating : {trip.rating}
+                    <br />
+                    Difficulty : {trip.difficulty}
+                    <br />
+                    Details : {trip.details}
+                  </p>
                 </div>
-                <img
-                  className="img-fluid rounded mb-5"
-                  src={trip.img}
-                  alt="..."
-                />
-                <p className="mb-4">
-                  City : {trip.city}
-                  <br />
-                  Length : {trip.length1}Km
-                  <br />
-                  Rating : {trip.rating}
-                  <br />
-                  Difficulty : {trip.difficulty}
-                  <br />
-                  Details : {trip.details}
-                </p>
               </div>
             </div>
           </div>
         </div>
+
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default TripDetail;
